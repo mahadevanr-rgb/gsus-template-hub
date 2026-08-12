c519ef3272e84ea95e110371fbb463d111866a47
Mahadevan205 <r.mahadevan@gsusstarsoftware.com>
2026-08-12 09:43:12 +0530

    feat: supabase sync banner + dashboard integration + RLS policies

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,14 +1,12 @@
 import MainLayout from "../components/layout/MainLayout";
-import DashboardStats from "../components/organisms/DashboardStats";
 import TemplatesSection from "../components/organisms/TemplatesSection";
 import ComponentsSection from "../components/organisms/ComponentsSection";
-import ComponentsNavigation from "../components/organisms/ComponentsNavigation";
+import SyncBanner from "../components/organisms/SyncBanner";
 
 export default function DashboardPage({ onNavigate, currentPage }) {
   return (
     <MainLayout onNavigate={onNavigate} currentPage={currentPage}>
-      {/* <DashboardStats /> */}
-      {/* <ComponentsNavigation onNavigate={onNavigate} /> */}
+      <SyncBanner />
       <TemplatesSection onNavigate={onNavigate} />
       <ComponentsSection onNavigate={onNavigate} />
     </MainLayout>
